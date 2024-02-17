ee8e13edb4ca546b73a3d2feaf390ef7cb873db2
ChristopherGS <christopher.samiullah@gmail.com>
2024-02-17 13:05:53 +0000

    ready for the data engineering part

---
+++ project_podcast_summarizer/data_engineering/summarize_transcript.py
@@ -0,0 +1,13 @@
+
+def summarize_transcript(transcript: str) -> None:
+    ...
+
+
+def save_summary(episode_id: int, summary: str) -> None:
+    ...
+
+
+def main(episode_name: str) -> None:
+    episode_id = # fetch episode
+
+    #
