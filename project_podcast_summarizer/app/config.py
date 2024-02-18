b86a74b261045f7189171625ef63c2139135e59e
ChristopherGS <christopher.samiullah@gmail.com>
2024-02-18 12:19:16 +0000

    load llm for data engineering with typer

---
+++ project_podcast_summarizer/app/config.py
@@ -0,0 +1,42 @@
+import pathlib
+
+from pydantic import AnyHttpUrl, validator
+from pydantic_settings import BaseSettings
+from typing import List, Optional, Union
+
+
+# Project Directories
+ROOT = pathlib.Path(__file__).resolve().parent.parent
+PROMPT_DIR = ROOT / 'data_engineering' / 'prompts'
+TRANSCRIPT_DIR = ROOT / 'data' / 'transcripts'
+MODEL_DIR = ROOT / 'data_engineering' / 'models'
+
+class LLMSettings(BaseSettings):
+    CONTEXT_WINDOW: int = 512
+    N_GPU_LAYERS: int = 1
+    MODEL_FILE_NAME: str = "tinyllama-1.1b-chat-v0.3.Q2_K.gguf"  # TODO: update
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
+    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    SQLALCHEMY_DATABASE_URI: Optional[str] = "sqlite:///example.db"
+    llm: LLMSettings = LLMSettings()
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
--- project_podcast_summarizer/data_engineering/summarize_transcript.py
+++ project_podcast_summarizer/data_engineering/summarize_transcript.py
@@ -1,13 +1,79 @@
+import json
+from pathlib import Path
+from typing import Iterator
 
-def summarize_transcript(transcript: str) -> None:
-    ...
+from llama_cpp import (
+    Llama,
+    CreateChatCompletionResponse,
+    CreateChatCompletionStreamResponse,
+    LlamaGrammar,
+)
+import typer
+
+from app.config import MODEL_DIR, PROMPT_DIR, TRANSCRIPT_DIR
+from app.config import settings
+
+
+app = typer.Typer()
+
+def load_system_prompt() -> str:
+    with open(settings.PROMPT_DIR / "summarize_podcast_transcript.md.j2", "r") as file:
+        return file.read()
+
+def prepare_user_prompt(transcript_path: Path) -> str:
+    with open(transcript_path, "r") as file:
+        transcript = json.load(file)
+
+    return json.dumps(transcript)
+
+
+def load_model(model_path: Path) -> Llama:
+    return Llama(
+        model_path=str(model_path),
+        n_ctx=settings.llm.CONTEXT_WINDOW,
+        n_gpu_layers=settings.llm.N_GPU_LAYERS,
+    )
+
+def prepare_output(
+    llm: Llama,
+    user_prompt: str,
+    system_prompt: str,
+    grammar: LlamaGrammar | None = None,
+) -> CreateChatCompletionResponse | Iterator[CreateChatCompletionStreamResponse]:
+    return llm.create_chat_completion(
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+        max_tokens=settings.LLM_MAX_TOKENS,
+        stop=[],
+        temperature=settings.TEMPERATURE,
+        grammar=grammar,
+    )
+
+def summarize_transcript(transcript: str, llm: Llama) -> None:
+
+    # Load prompt from jinja templates
+    system_prompt = load_system_prompt()
+
+    user_prompt = prepare_user_prompt(transcript_path=settings.TRANSCRIPT_DIR / transcript)
+
+    output = prepare_output(llm=llm, system_prompt=system_prompt, user_prompt=user_prompt)
+
 
 
 def save_summary(episode_id: int, summary: str) -> None:
     ...
 
+@app.command()
+def run_summary_pipeline(
+    model: str = typer.Option(settings.llm.MODEL_FILE_NAME, "--model", "-m", help="local llm"),
+    transcript_file_name: str = typer.Option(..., "--transcript-file", "-t", help="Transcript file name"),
+) -> None:
+    typer.echo(f"Loading model: {model}")
+    llm = load_model(model_path=MODEL_DIR / model)
+    typer.echo("Loaded LLM")
 
-def main(episode_name: str) -> None:
-    episode_id = # fetch episode
 
-    #
+if __name__ == "__main__":
+    app()
